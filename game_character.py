d1749ba900b8e7ce0c51e7a880c5a26eca1a2e5f
Morthais <girobyte98@gmail.com>
2021-10-22 13:25:27 -0600

    update screen with character at center

--- game_character.py
+++ game_character.py
@@ -15,6 +15,6 @@ class Character():
         self.rect.centerx = self.screen_rect.centerx                    # set the centerx of the character to that of the screen
         self.rect.centery = self.screen_rect.centery                    # set the centery of the character to that of the screen
 
-        def blitme(self):
-            """Draw the character at its current location"""
-            self.screen.blit(self.image, self.rect)
+    def blitme(self):
+        """Draw the character at its current location"""
+        self.screen.blit(self.image, self.rect)
