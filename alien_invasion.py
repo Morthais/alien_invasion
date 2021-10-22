016a988100617cefab9149c7f68fefbc93fe6cb1
Morthais <girobyte98@gmail.com>
2021-10-22 13:03:45 -0600

    refactor main loop, build game_functions module

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,10 +4,11 @@ Author: Jacob Farr (Morthais)
 Credits: "Python Crash Course" by Eric Matthes
 """
 # import packages
-import sys      # sys is used to exit game when player quits
 import pygame   # pygame contains functionality needed to make a game
+import game_functions as gf     # import game_functions I created as the alias gf
 from settings import Settings   # import Settings class I created from settings.py file/module
 from ship import Ship
+
 
 def run_game():
 
@@ -29,17 +30,8 @@ def run_game():
     # start the main loop for game
     while True:
         """This is the main loop for alien_invasion"""
-        # watch for keyboard and mouse events (as declared by pygame.event.get())
-        for event in pygame.event.get():                        # this is an event loop
-            if event.type == pygame.QUIT:                       # which listens for an event
-                sys.exit()                                      # and does a task if event occurs
-
-        # redraw the screen during each pass through the loop
-        screen.fill(ai_settings.bg_color)                       # use the pygame fill module and Settings class to set the background color
-        ship.blitme()
-
-        # make the most recently drawn screen visible (to create the illusion of smooth movement)
-        pygame.display.flip()                                   # draws an empty screen in this case
+        gf.check_events()
+        gf.update_screen(ai_settings, screen, ship)
 
 run_game()
 
---
+++ game_functions.py
@@ -0,0 +1,18 @@
+import sys      # sys is used to exit the game when player quits
+import pygame
+
+def check_events():
+    """Responds to keypresses and mouse events."""
+    # watch for keyboard and mouse events (as declared by pygame.event.get())
+    for event in pygame.event.get():                        # this is an event loop
+        if event.type == pygame.QUIT:                       # which listens for an event
+            sys.exit()                                      # and does a task if event occurs
+
+def update_screen(ai_settings, screen, ship):
+    """Update images on the screen and flip to the new screen."""
+    # redraw the screen during each pass through the loop
+    screen.fill(ai_settings.bg_color)                       # use the pygame fill module and Settings class to set the background color
+    ship.blitme()
+
+    # make the most recently drawn screen visible (to create the illusion of smooth movement)
+    pygame.display.flip()                                   # draws an empty screen in this case
